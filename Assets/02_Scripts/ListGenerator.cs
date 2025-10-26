f7e9c70ba5974f9ba6002f67a9349033a0b2907f
Connyhxd <fuhualover666@gmail.com>
2025-10-26 01:22:14 -0300

    ajustes al display de la lista

--- Assets/02_Scripts/ListGenerator.cs
+++ Assets/02_Scripts/ListGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ListGenerator : MonoBehaviour
 {
@@ -7,9 +8,12 @@ public class ListGenerator : MonoBehaviour
 
     public List<string> shoppingList = new List<string>();
 
+    public TMP_Text listText;
+
     private void Start()
     {
         GenerateList();
+        ShowShoppingList();
     }
 
     void GenerateList()
@@ -24,6 +28,35 @@ public class ListGenerator : MonoBehaviour
             shoppingList.Add(randomItem);
         }
 
+    }
+
+
+    void ShowShoppingList()
+    {
+        Dictionary<string, int> itemCounts = new Dictionary<string, int>();
+
+        foreach (string item in shoppingList)
+        {
+            if (itemCounts.ContainsKey(item))
+                itemCounts[item]++;
+            else
+                itemCounts[item] = 1;
+        }
+
+        string displayText = "Lista de compras:\n\n";
+
+        foreach (KeyValuePair<string, int> entry in itemCounts)
+        {
+            if (entry.Value > 1)
+                displayText += $"• {entry.Key} x{entry.Value}\n";
+            else
+                displayText += $"• {entry.Key}\n";
+        }
+
+        if (listText != null)
+        {
+            listText.text = displayText;
+        }
     }
 
 }
