769e56f8ffb87306a00d87fc78eafa83f0d88dc0
Connyhxd <fuhualover666@gmail.com>
2025-10-26 18:49:33 -0300

    boleta prototipo¿

--- Assets/02_Scripts/Inventory.cs
+++ Assets/02_Scripts/Inventory.cs
@@ -1,44 +1,84 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class Inventory : MonoBehaviour
 {
-    public List<Item> itemsInCart = new List<Item>();
+    public List<ItemTemplate> itemsInCart = new List<ItemTemplate>();
 
-    private ListGenerator listGenerator;
+    public ListGenerator listGenerator;
+
+    public TextMeshProUGUI boletaText;
 
     private void OnTriggerEnter(Collider other)
     {
         Item item = other.GetComponent<Item>();
-        if (item != null && !itemsInCart.Contains(item))
+        if (item != null && item.itemTemplate && !itemsInCart.Contains(item.itemTemplate))
         {
-            itemsInCart.Add(item);
+            itemsInCart.Add(item.itemTemplate);
             CheckProgress();
+            Boleta();
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         Item item = other.GetComponent<Item>();
-        if (item != null && itemsInCart.Contains(item))
+        if (item != null && itemsInCart.Contains(item.itemTemplate))
         {
-            itemsInCart.Remove(item);
+            itemsInCart.Remove(item.itemTemplate);
             CheckProgress();
+            Boleta();
         }
     }
 
-    private void CheckProgress()
+    void CheckProgress()
     {
         int correctItems = 0;
 
-        foreach (Item item in itemsInCart)
+        foreach (ItemTemplate item in itemsInCart)
         {
-            if (listGenerator.IsItemInShoppingList(item.itemTemplate))
+            if (listGenerator.IsItemInShoppingList(item))
             {
                 correctItems++;
             }
         }
     }
 
+    void Boleta()
+    {
+
+        Dictionary<ItemTemplate, int> itemCounts = new Dictionary<ItemTemplate, int>();
+
+        int total = 0;
+        string receipt = "BOLETA\n\n";
+
+        foreach (ItemTemplate item in itemsInCart)
+        {
+            if (itemCounts.ContainsKey(item))
+                itemCounts[item]++;
+            else
+                itemCounts[item] = 1;
+        }
+
+        foreach (KeyValuePair<ItemTemplate, int> kvp in itemCounts)
+        {
+            ItemTemplate item = kvp.Key;
+            int count = kvp.Value;
+            int subtotal = item.itemPrice * count;
+
+            receipt += item.itemName + " x" + count + " - $" + subtotal + "\n";
+            total += subtotal;
+        }
+
+        receipt += "\nTOTAL: $" + total.ToString();
+
+        if (boletaText != null)
+        {
+            boletaText.text = receipt;
+        }
+
+    }
 
 }
