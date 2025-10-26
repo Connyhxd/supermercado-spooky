3c395a4e21e8b228e2046820f29df9eb01752516
Connyhxd <fuhualover666@gmail.com>
2025-10-26 01:47:22 -0300

    scriptable objects y cositas
    ola nose si leas esto pero es pa empezar a hacer lo del inventario y el coso para que salga la info del producto yyyeso

---
+++ Assets/02_Scripts/ItemTemplate.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewItem", menuName = "ScriptableObjects/Item")]
+public class ItemTemplate : ScriptableObject
+
+{
+    public string itemName;
+    public string itemType;
+    public int itemPrice;
+}
