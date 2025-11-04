9e6dc9e6285b6fdbd40a11df3e2bafb96fa94344
Connyhxd <fuhualover666@gmail.com>
2025-11-04 18:01:06 -0300

    dialogo inicial aorasi

---
+++ Assets/02_Scripts/SceneDialoguesManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneDialoguesManager : MonoBehaviour
+{
+    public DialogueScriptable dialogue;
+    public TextMeshProUGUI dialogueText, nameText;
+    public Image spriteCharacter;
+
+    public bool isTalking;
+    public int phraseIndex;
+
+    public bool isTypeWriterEnded;
+
+    private void Start()
+    {
+        Talk();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Talk();
+        }
+    }
+
+    public void Talk()
+    {
+        if (isTalking)
+        {
+            if (phraseIndex < dialogue.dialogue.Length - 1)
+            {
+                if (isTypeWriterEnded)
+                {
+                    phraseIndex++;
+                    Refresh();
+                }
+                else
+                {
+                    StopCoroutine("TypeWriter");
+                    dialogueText.text = dialogue.dialogue[phraseIndex].phrase;
+                    isTypeWriterEnded = true;
+                }
+            }
+            else
+            {
+                if (isTypeWriterEnded)
+                {
+                    EndDialogue();
+                }
+                else
+                {
+                    StopCoroutine("TypeWriter");
+                    dialogueText.text = dialogue.dialogue[phraseIndex].phrase;
+                    isTypeWriterEnded = true;
+                }
+            }
+        }
+        else
+        {
+            if (dialogue != null)
+            {
+                Refresh();
+                isTalking = true;
+            }
+        }
+    }
+
+    public void Refresh()
+    {
+        spriteCharacter.sprite = dialogue.dialogue[phraseIndex].characterSprite;
+        nameText.text = dialogue.dialogue[phraseIndex].characterName;
+        StartCoroutine("TypeWriter");
+    }
+
+    private IEnumerator TypeWriter()
+    {
+        isTypeWriterEnded = false;
+        dialogueText.text = string.Empty;
+        foreach (char text in dialogue.dialogue[phraseIndex].phrase)
+        {
+            dialogueText.text += text;
+            yield return new WaitForSeconds(0.1f);
+        }
+        isTypeWriterEnded = true;
+    }
+
+    public void EndDialogue()
+    {
+        StopAllCoroutines();
+        nameText.text = string.Empty;
+        dialogueText.text = string.Empty;
+        spriteCharacter.sprite = null;
+        phraseIndex = 0;
+        isTalking = false;
+        isTypeWriterEnded = true;
+    }
+}
